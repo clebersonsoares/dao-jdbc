64dc5755825e597c97c643a839006acb9f1b27e1
clebersonsoares <clebersonxadrez@hotmail.com>
2022-05-07 07:23:07 -0300

    Criado a entidade Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,34 +1,12 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import db.DB;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection con = null;
-		Statement st = null;
-		try {
-			con = DB.getConnection();
-			con.setAutoCommit(false);
-			st = con.createStatement();
-			st.executeUpdate("update seller set BaseSalary = 1090 where DepartmentId = 2");
-			st.executeUpdate("update seller set BaseSalary = 9000 where DepartmentId = 1");
-			con.commit();
-			
-		}catch(SQLException e) {
-			try {
-				con.rollback();
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			};
-		}finally {
-			DB.closeStatement(st);
-			DB.closeConnection(con);
-		}
+	Department b = new Department(1, "Books");
+	System.out.println(b);
 
 	}
 
