1477df18ae1841155ef7e62f08069dc5b45976fb
clebersonsoares <clebersonxadrez@hotmail.com>
2022-05-07 13:00:08 -0300

    Criação da entidade Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 	Department b = new Department(1, "Books");
-	System.out.println(b);
+	Seller seller = new Seller(1,"Thomas","thomas@gmail.com",new Date(),5000.0, b);
+	System.out.println(seller);
 
 	}
 
