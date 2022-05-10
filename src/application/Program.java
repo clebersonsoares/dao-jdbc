803b9eea52e1949493fe11535e504e283f571dcd
clebersonsoares <clebersonxadrez@hotmail.com>
2022-05-10 06:24:06 -0300

    Criado as funções de instanciação de Department e Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,13 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-	Department b = new Department(1, "Books");
-	Seller seller = new Seller(1,"Thomas","thomas@gmail.com",new Date(),5000.0, b);
-	
+	System.out.println("====  TEST FINDBYID  ====");
 	SellerDao sellerDao = DaoFactory.createSellerDao();
 	Seller se = sellerDao.findById(1);
 	System.out.println(se);
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -13,25 +13,27 @@ import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
 	private Connection con;
+
 	public SellerDaoJDBC(Connection conn) {
 		this.con = conn;
 	}
+
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -39,36 +41,48 @@ public class SellerDaoJDBC implements SellerDao {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public Seller findById(Integer id) {
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		try {
-			ps = con.prepareStatement("SELECT seller.*,department.Name as DepName FROM seller INNER JOIN department ON seller.DepartmentId = department.Id WHERE seller.Id = ?");
-			ps.setInt(1,id);
+			ps = con.prepareStatement(
+					"SELECT seller.*,department.Name as DepName FROM seller INNER JOIN department ON seller.DepartmentId = department.Id WHERE seller.Id = ?");
+			ps.setInt(1, id);
 			rs = ps.executeQuery();
-			if(rs.next()) {
-				Department dep = new Department();
-				dep.setId(rs.getInt("Id"));
-				dep.setName(rs.getString("depName"));
-				Seller seller = new Seller();
-				seller.setId(rs.getInt("Id"));
-				seller.setName(rs.getString("Name"));
-				seller.setEmail(rs.getString("Email"));
-				seller.setBirthDate(rs.getDate("BirthDate"));
-				seller.setBaseSalary(rs.getDouble("BaseSalary"));
-				seller.setDepartment(dep);
+			if (rs.next()) {
+				Department dep = instaciateDepartment(rs);
+				Seller seller = instanciateSeller(rs,dep);
 				return seller;
 			}
 
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			System.out.println(e.getMessage());
-		}finally {
+		} finally {
 			DB.closeResultSet(rs);
 			DB.closeStatement(ps);
 		}
 		return null;
-		
+
+	}
+
+	private Seller instanciateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller seller = new Seller();
+		seller.setId(rs.getInt("Id"));
+		seller.setName(rs.getString("Name"));
+		seller.setEmail(rs.getString("Email"));
+		seller.setBirthDate(rs.getDate("BirthDate"));
+		seller.setBaseSalary(rs.getDouble("BaseSalary"));
+		seller.setDepartment(dep);
+		return seller;
+	}
+
+	private Department instaciateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("Id"));
+		dep.setName(rs.getString("depName"));
+		return dep;
 	}
 
 }
